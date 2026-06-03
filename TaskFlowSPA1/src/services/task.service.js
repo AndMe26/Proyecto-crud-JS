913bb8bb1f1ac4194bb47fb6dc34682eab4750dd
andres mena <andresmena284@gmail.com>
2026-06-03 15:54:07 -0500

    feat: se construye las lineas de codigo de task.service.js

---
+++ TaskFlowSPA1/src/services/task.service.js
@@ -0,0 +1,37 @@
+
+
+const API_URL = "http://localhost:3000/tasks"
+
+export async function crearTarea(tarea) {
+    const response = await fetch(API_URL, {
+        method: "POST" ,
+        headers: { "Content-Type" : "application/json" },
+        body: JSON.stringify(tarea)
+    });
+    if (!response.ok) throw new Error("Error al crear tarea");
+    return await response.json()
+}
+
+export async function obtenerTareas(userId) {
+    const response = await fetch(`${API_URL}?userID=${userId}`)
+    if (!response.ok) throw new Error("Error al obtener tareas")
+        return await response.json();
+}
+
+export async function actualizarTarea(id,datos) {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT" ,
+        headers: { "Content-Type" : "application/json" },
+        body: JSON.stringify(datos)
+    });
+    if (!response.ok) throw new Error("Error al actualizar tarea");
+    return await response.json();
+}
+
+export async function eliminarTarea(id){
+    const response = await fetch(`${API_URL}/${id}`,{
+        method: "DELETE"
+    });
+    if (!response.ok) throw new Error("Error al eliminar tarea");
+    return await response.json();
+}
